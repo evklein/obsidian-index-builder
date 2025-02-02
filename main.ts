2b42dd950d8fbea488e56602ebb222be8cf7cdc4
hasherr <eklein217@gmail.com>
2025-02-02 00:38:21 -0500

    Indexing is working now - very barebones

--- main.ts
+++ main.ts
@@ -16,39 +16,76 @@ export default class IndexBuilderPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-        console.log('fuck!');
-        console.log(getIconIds());
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon('lucide-blocks', 'Build Indices', async (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
 			new Notice('This is a notice!');
 
-            const lectureNoteDocs = app.plugins.plugins.dataview.api
+            const indexFiles = app.plugins.plugins.dataview.api
                 .pages()
                 .where(page => {
-                    return page.doctype &&
-                        page.doctype == "grad-school-lecture-note" &&
-                        page.class &&
-                        page.class == "S580";
+                    return page.doctype && page.doctype === 'index';
+                });
+            
+            indexFiles.forEach(async indexFile => {
+let fileContents = `---
+doctype: ${indexFile.doctype}
+curates: ${indexFile.curates}
+idx_filter_property: ${indexFile.idx_filter_property}
+idx_filter_value: ${indexFile.idx_filter_value}
+idx_columns:\n${indexFile.idx_columns.reduce((list: string, c: string) => list + `  - ${c}\n`, '\n')}
+idx_columns_as:\n${indexFile.idx_columns_as.reduce((list: string, c: string) => list + `  - ${c}\n`, '\n')}
+---\n`;
+console.log('hmmmmm');
+                const curatedFilesForIndex = app.plugins.plugins.dataview.api
+                    .pages()
+                    .where(page => {
+                        return page.doctype &&
+                            page.doctype === indexFile.curates &&
+                            page[page.idx_filter_property] === page.idx_filter_value
+                    });
+
+                fileContents += `${indexFile.idx_columns_as.join('|')}\n`
+                fileContents += new Array(indexFile.idx_columns.length).fill('--').join('|') + '\n';
+                curatedFilesForIndex.forEach(doc => {
+                    fileContents += `${doc.unit} | ${doc.subunit} | [[${doc.file.link.path}\\|${doc.file.name}]] | ${doc.lecturers?.join(", ")}\n`;
                 });
 
-            let results = "Lec. No. | Topic | Lecturer(s) | Tags\n";
-            results += "-- | -- | -- | --\n";
-            lectureNoteDocs.sort(doc => Number(doc.subunit), "asc")
-                        .forEach((doc) => {
-                console.log(doc);
-                results += `${doc.subunit} | [[${doc.file.link.path}\\|${doc.file.name}]] | ${doc.lecturers.join(", ")} | hm \n`; // ${doc.tags.join(", ")}
+                try {
+                    await this.app.vault.adapter.write(indexFile.file.path, fileContents);
+                    console.log('writing done.');
+                }
+                catch (error) {
+                    console.error('I cant :(');
+                }
             });
-            console.log(results);
 
-            const filePath = "grad-school/S580/⭐ Index S580.md";
-            try {
-                await this.app.vault.adapter.write(filePath, results);
-                console.log('writing done.');
-            }
-            catch (error) {
-                console.error('I cant :(');
-            }
+            // const lectureNoteDocs = app.plugins.plugins.dataview.api
+            //     .pages()
+            //     .where(page => {
+            //         return page.doctype &&
+            //             page.doctype == "grad-school-lecture-note" &&
+            //             page.class &&
+            //             page.class == "S580";
+            //     });
+
+            // let results = "Lec. No. | Topic | Lecturer(s) | Tags\n";
+            // results += "-- | -- | -- | --\n";
+            // lectureNoteDocs.sort(doc => Number(doc.subunit), "asc")
+            //             .forEach((doc) => {
+            //     console.log(doc);
+            //     results += `${doc.subunit} | [[${doc.file.link.path}\\|${doc.file.name}]] | ${doc.lecturers.join(", ")} | hm \n`; // ${doc.tags.join(", ")}
+            // });
+            // console.log(results);
+
+            // const filePath = "grad-school/S580/⭐ Index S580.md";
+            // try {
+            //     await this.app.vault.adapter.write(filePath, results);
+            //     console.log('writing done.');
+            // }
+            // catch (error) {
+            //     console.error('I cant :(');
+            // }
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
